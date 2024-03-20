a85599c485d0f1c1947c9b99b79c7e1be653bc84
Siravij Praevisavakij <siravijbb@gmail.com>
2024-03-20 16:00:39 +0700

    fireee

--- src/routes/api/add/+server.ts
+++ src/routes/api/add/+server.ts
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-let TotalPop = 2837;
+let TotalPop = 3100;
 /// STATE
 /// 0:Terminated Service at Station
 /// 1:Arriving Station
